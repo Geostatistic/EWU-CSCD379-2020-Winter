c8581f2acda6304bbc57b34726292c6c93388c92
Kristofer Veselits <kveselits@eagles.ewu.edu>
2020-01-24 00:41:37 -0800

    Typos

--- SecretSanta/GlobalSuppressions.cs
+++ SecretSanta/GlobalSuppressions.cs
@@ -7,5 +7,5 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Given code that should not be changed.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "Given code not intended as general purpose.")]
 //SecretSanta.Business
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Compiler wants url to not be a string, but supressing this to stay within assignment requirements.")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Compiler wants url to not be a string, but supressing this to stay within assignment requirements.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Compiler wants url to not be a string, but suppressing this to stay within assignment requirements.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Compiler wants url to not be a string, but suppressing this to stay within assignment requirements.")]
